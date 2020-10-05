c36fd55e8eb8a03db56f98d5be2fef3c0a0f7f17
茗少 <1753972608@qq.com>
2020-10-05 15:23:11 +0800

    细节修改

--- src/main/java/cclub/demo/dao/Rand.java
+++ src/main/java/cclub/demo/dao/Rand.java
@@ -7,7 +7,7 @@ public class Rand {
         StringBuffer buffer=new StringBuffer("");
         Random rand=new Random();
         for(int i=0;i<6;i++){
-            buffer.append(rand.nextInt(6));
+            buffer.append(rand.nextInt(10));
         }
         return buffer.toString();
     }
